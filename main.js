f4d2131eaca0194fb12650a1c5fd76a90407afad
israel <israelwendellc@gmail.com>
2023-07-12 18:10:28 -0300

    excluindo observações

--- main.js
+++
@@ -1,6 +0,0 @@
-const carroselImagens = document.querySelector (".carrosel_imagens");
-
-function showSlide(index) {
-    carroselImagens.style.transform = `translateX(-${index * 100}%)`;
-    console.log (carroselImagens)
-  }
